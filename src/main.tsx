ee87a027d6b629e6ed5e8f2101c536cfb7b2a45a
EmirDelicR <emirdelictbf@gmail.com>
2023-04-26 13:56:59 +0200

    Clean up main and utils

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './UI/App';
 import '@plugins/i18n/i18n.ts';
-// import '@/styles/app.scss';
+
+import App from './UI/App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- src/utils/menuHelpers.ts
+++ src/utils/menuHelpers.ts
@@ -1,69 +1,49 @@
-/**
- * Add - remove transition event from HTML element
- * @param {HTMLElement} el
- * @param {Function} callback
- */
-const endTransitionHandler = (el: HTMLElement, callback: () => void) => {
-  const onEndCallbackFn = (ev: any) => {
-    if (ev.target != el) return;
-    el.removeEventListener('transitionend', onEndCallbackFn);
+const endTransitionHandler = (element: HTMLElement, callback: () => void) => {
+  const onEndCallbackFn = (event: TransitionEvent) => {
+    if (event.target != element) return;
+    element.removeEventListener('transitionend', onEndCallbackFn);
 
     if (callback && typeof callback === 'function') {
       callback();
     }
   };
 
-  el.addEventListener('transitionend', onEndCallbackFn);
+  element.addEventListener('transitionend', onEndCallbackFn);
 };
 
-/**
- *
- * Gets the current stack pages indexes.
- * If any of them is the exclude then this
- * one is not part of the returned array
- *
- * @param {Number} current  Current page index
- * @param {Number} pages Number of pages
- * @param {Number} exclude Page index to exclude
- */
-
 const getStackPages = (
-  current: number,
-  pages: number,
-  exclude: number | null = null
+  currentPageIndex: number,
+  numberOfPages: number,
+  indexToExclude: number | null = null
 ) => {
-  const firstPage = current + 1 < pages ? current + 1 : 0;
-  const secondPage = current + 2 < pages ? current + 2 : 1;
+  const firstPage =
+    currentPageIndex + 1 < numberOfPages ? currentPageIndex + 1 : 0;
+  const secondPage =
+    currentPageIndex + 2 < numberOfPages ? currentPageIndex + 2 : 1;
 
   const pageIndexes = [];
 
-  if (exclude != current) {
-    pageIndexes.push(current);
+  if (indexToExclude != currentPageIndex) {
+    pageIndexes.push(currentPageIndex);
   }
 
-  if (exclude != firstPage) {
+  if (indexToExclude != firstPage) {
     pageIndexes.push(firstPage);
   }
 
-  if (exclude != secondPage) {
+  if (indexToExclude != secondPage) {
     pageIndexes.push(secondPage);
   }
 
   return pageIndexes;
 };
 
-/**
- * Build page stack when press nav button
- * @param {Number} current Current page index
- * @param {Number} numberOfPages
- * @param {Array} pages Array of HTML elements
- */
 const buildPageStack = (
-  current: number,
+  pageIndex: number,
   numberOfPages: number,
   pages: HTMLElement[]
 ) => {
-  const stackPages = getStackPages(current, numberOfPages);
+  const stackPages = getStackPages(pageIndex, numberOfPages);
   /**
    * set z-index, opacity, initial transforms to pages
    * and add class page--inactive to all except the current one
@@ -72,13 +52,14 @@ const buildPageStack = (
     const page = pages[i];
     const position = stackPages.indexOf(i);
 
-    if (current !== i) {
+    if (pageIndex !== i) {
       page.classList.add('page--inactive');
       page.style.transform = getTransformValue(position);
     } else {
       page.classList.remove('page--inactive');
     }
-    const zIndex = i < current ? current - i : numberOfPages + current - i;
+    const zIndex =
+      i < pageIndex ? pageIndex - i : numberOfPages + pageIndex - i;
     page.style.zIndex = String(zIndex);
 
     page.style.opacity = String(getOpacityValue(position));
@@ -87,10 +68,12 @@ const buildPageStack = (
 
 export { endTransitionHandler, getStackPages, buildPageStack };
 
-const getTransformValue = (pos: number) => {
-  return pos !== -1 ? 'translate3d(0,100%,0)' : 'translate3d(0,75%,-300px)';
+const getTransformValue = (position: number) => {
+  return position !== -1
+    ? 'translate3d(0,100%,0)'
+    : 'translate3d(0,75%,-300px)';
 };
 
-const getOpacityValue = (pos: number) => {
-  return pos !== -1 ? parseFloat(String(1 - 0.1 * pos)) : 0;
+const getOpacityValue = (position: number) => {
+  return position !== -1 ? parseFloat(String(1 - 0.1 * position)) : 0;
 };
