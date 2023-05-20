51d574d5c8ec2a5312c03d96833a5a0f20e95df2
EmirDelicR <emirdelictbf@gmail.com>
2023-05-20 09:36:52 +0200

    Add typed text

--- src/UI/pages/home/Home.tsx
+++ src/UI/pages/home/Home.tsx
@@ -1,3 +1,17 @@
+import TextTyping from '@/UI/components/textTyping/TextTyping';
+import './Home.scss';
+
 export default function Home() {
-  return <span>Home</span>;
+  return (
+    <div className="home-wrapper">
+      <div>
+        <div>Hello</div>
+        <h3>Wrting text</h3>
+        <TextTyping />
+        <br />
+        <button>Download CV</button>
+      </div>
+      <div>Image</div>
+    </div>
+  );
 }
