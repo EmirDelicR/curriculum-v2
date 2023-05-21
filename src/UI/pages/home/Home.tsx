5561a72845e4802afdf76cdbffd82bd6a97a7b75
EmirDelicR <emirdelictbf@gmail.com>
2023-05-21 12:12:55 +0200

    Added some css

--- src/UI/pages/home/Home.tsx
+++ src/UI/pages/home/Home.tsx
@@ -8,54 +8,44 @@ import './Home.scss';
 
 export default function Home() {
   const { t } = useTranslation();
+
   return (
     <div className="home-wrapper">
-      <div>
+      <div className="description">
+        <p>{t('my-description.introduce')}</p>
+        <TextTyping />
         <WaterMark text="hello" />
-        <div className="my-description">
-          <p>{t('my-description.introduce')}</p>
-          <p>
-            <Trans
-              i18nKey="my-description.source"
-              components={{
-                CodeLink: (
-                  <a
-                    className="highlight"
-                    href="https://github.com/EmirDelicR/curriculum"
-                    target="_blank"
-                    rel="noreferrer"
-                  />
-                )
-              }}
-            />
-          </p>
-          <p>
-            <Trans
-              i18nKey="my-description.mail"
-              components={{
-                MailLink: (
-                  <a
-                    className="highlight"
-                    href="mailto:emirdelictbf@gmail.com?subject=Web-site mail"
-                  />
-                )
-              }}
-            />
-          </p>
-        </div>
-        {/* <TextTyping /> */}
-        <br />
+        <p className="text">
+          <Trans
+            i18nKey="my-description.source"
+            components={{
+              CodeLink: (
+                <a
+                  className="highlight"
+                  href="https://github.com/EmirDelicR/curriculum"
+                  target="_blank"
+                  rel="noreferrer"
+                />
+              )
+            }}
+          />
+        </p>
+        <p className="text">
+          <Trans
+            i18nKey="my-description.mail"
+            components={{
+              MailLink: (
+                <a
+                  className="highlight"
+                  href="mailto:emirdelictbf@gmail.com?subject=Web-site mail"
+                />
+              )
+            }}
+          />
+        </p>
         <DownloadButton />
       </div>
-      <div>
-        <img
-          src={`${window.location.origin}/images/assets/me.png`}
-          loading="lazy"
-          alt="image of me"
-          width="400"
-          height="400"
-        />
-      </div>
+      <div className="main-img-wrapper"></div>
     </div>
   );
 }
