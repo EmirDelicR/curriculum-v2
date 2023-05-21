085301c6eb4d410ad81a97068296a472477c3fd0
EmirDelicR <emirdelictbf@gmail.com>
2023-05-21 09:47:46 +0200

    Deed download button

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -39,3 +39,10 @@ export const SOCIAL_PAGES = [
     download: 'CV.pdf'
   }
 ];
+
+const LINKS = {
+  company:
+    '<a class="language-cour" href="https://www.x-net.at/de" target="_blank" rel="noreferrer">X-net Service</a>',
+  git: ' <a class="language-cour" href="https://github.com/EmirDelicR/curriculum" target="_blank" rel="noreferrer">code</a>',
+  mail: '<a class="language-cour" href="mailto:emirdelictbf@gmail.com?subject=Web-site mail">emirdelictbf@gmail.com</a>'
+};
