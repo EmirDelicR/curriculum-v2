7b35a9e49b0418e41df6fe4c8dcec6f105e21f36
EmirDelicR <emirdelictbf@gmail.com>
2023-06-04 18:42:01 +0200

    Working with accordion

--- src/UI/components/accordion/Accordion.tsx
+++ src/UI/components/accordion/Accordion.tsx
@@ -13,6 +13,60 @@ interface Props {
 
 import './Accordion.scss';
 
+// export default function Accordion({ data }: Props) {
+//   const accordionRef = useRef<HTMLDivElement>(null);
+
+//   const onAccordionPanelClick = (position: number) => () => {
+//     const buttons = accordionRef.current?.querySelectorAll('button');
+//     const contents =
+//       accordionRef.current?.querySelectorAll('.accordion-content');
+
+//     buttons!.forEach((button) => {
+//       button.setAttribute('aria-expanded', 'false');
+//     });
+
+//     contents!.forEach((content) => {
+//       content.setAttribute('aria-hidden', 'true');
+//     });
+
+//     buttons![position].setAttribute('aria-expanded', 'true');
+//     contents![position].setAttribute('aria-hidden', 'false');
+//   };
+
+//   return (
+//     <div className="accordion" ref={accordionRef}>
+//       {data.map(({ title, imageUrl }, index) => (
+//         <div
+//           key={title}
+//           className="accordion-panel"
+//           onClick={onAccordionPanelClick(index)}
+//         >
+//           <button
+//             className="accordion-trigger"
+//             aria-controls={`panel${index}-content`}
+//             aria-expanded="true"
+//           ></button>
+//           <div
+//             className="accordion-content"
+//             id="panel1-content"
+//             aria-labelledby={`panel${index}-heading`}
+//             aria-hidden="false"
+//             role="region"
+//           >
+//             <h2>{title}</h2>
+//             <img
+//               className="accordion-image"
+//               src={imageUrl}
+//               alt="Accordion image"
+//               loading="lazy"
+//             />
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
 export default function Accordion({ data }: Props) {
   const accordionRef = useRef<HTMLDivElement>(null);
 
@@ -35,39 +89,15 @@ export default function Accordion({ data }: Props) {
 
   return (
     <div className="accordion" ref={accordionRef}>
-      {data.map(({ title, imageUrl, description }, index) => (
+      {data.map(({ title, imageUrl }, index) => (
         <div
           key={title}
-          className="accordion-panel"
-          onClick={onAccordionPanelClick(index)}
+          className="slide"
+          style={{ backgroundImage: `url(${imageUrl})` }}
         >
-          <h2 id={`panel${index}-heading`}>
-            <button
-              className="accordion-trigger"
-              aria-controls={`panel${index}-content`}
-              aria-expanded="true"
-            >
-              <span className="accordion-title" id={`panel${index}-title`}>
-                {title}
-              </span>
-              {/* <Icon></Icon> */}
-            </button>
-          </h2>
-
-          <div
-            className="accordion-content"
-            id="panel1-content"
-            aria-labelledby={`panel${index}-heading`}
-            aria-hidden="false"
-            role="region"
-          >
-            <p>{description}</p>
-            <img
-              className="accordion-image"
-              src={imageUrl}
-              alt="Accordion image"
-              loading="lazy"
-            />
+          <div className="overlay"></div>
+          <div className="content">
+            <h2>{title}</h2>
           </div>
         </div>
       ))}
