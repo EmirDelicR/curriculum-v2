fcb872bde16c3409035e42bbfa0707c076f26445
EmirDelicR <emirdelictbf@gmail.com>
2023-06-07 12:05:30 +0200

    Fix toggle

--- src/UI/components/buttons/toggleMode/ToggleMode.tsx
+++ src/UI/components/buttons/toggleMode/ToggleMode.tsx
@@ -1,28 +1,41 @@
-import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import './ToggleMode.scss';
-import { useState } from 'react';
 import { classNameHelper } from '@/utils/generalHelpers';
+
+import './ToggleMode.scss';
 
 interface Props {
   onClick: () => void;
 }
 
 export default function ToggleMode({ onClick }: Props) {
-  const [isLightMode, setIsLightMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
   const onClickHandler = () => {
-    setIsLightMode((prev) => !prev);
+    setIsDarkMode((prev) => !prev);
     onClick();
   };
 
   return (
-    <FontAwesomeIcon
-      className={classNameHelper('mode', isLightMode ? 'light' : '')}
-      icon={faLightbulb}
-      size="2x"
-      onClick={onClickHandler}
-    />
+    <div className="mode">
+      <div
+        className={classNameHelper('icon-wrapper', isDarkMode ? 'dark' : '')}
+      >
+        <FontAwesomeIcon
+          className="mode-icon sun"
+          icon={faSun}
+          size="2x"
+          onClick={onClickHandler}
+        />
+        <FontAwesomeIcon
+          className="mode-icon moon"
+          icon={faMoon}
+          size="2x"
+          onClick={onClickHandler}
+        />
+      </div>
+    </div>
   );
 }
