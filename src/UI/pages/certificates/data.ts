0f861af287391f1926d99d0d9be0c0a279fa936e
EmirDelicR <emirdelictbf@gmail.com>
2023-06-18 11:49:54 +0200

    Convert images

--- src/UI/pages/certificates/data.ts
+++ src/UI/pages/certificates/data.ts
@@ -3,52 +3,52 @@ import { AccordionItem } from '@/UI/components/accordion/Accordion';
 export const CERTIFICATIONS: AccordionItem[] = [
   {
     title: 'Frontend',
-    imageUrl: 'images/assets/certificates/FCC.jpg',
+    imageUrl: 'images/assets/certificates/FCC.webp',
     icon: null
   },
   {
     title: 'Java Script',
-    imageUrl: 'images/assets/certificates/JS.jpg',
+    imageUrl: 'images/assets/certificates/JS.webp',
     icon: null
   },
   {
     title: 'Node.js',
-    imageUrl: 'images/assets/certificates/Node.jpg',
+    imageUrl: 'images/assets/certificates/Node.webp',
     icon: null
   },
   {
     title: 'Progressive Web App',
-    imageUrl: 'images/assets/certificates/PWA.jpg',
+    imageUrl: 'images/assets/certificates/PWA.webp',
     icon: null
   },
   {
     title: 'Python and Django',
-    imageUrl: 'images/assets/certificates/Python.jpg',
+    imageUrl: 'images/assets/certificates/Python.webp',
     icon: null
   },
   {
     title: 'Python',
-    imageUrl: 'images/assets/certificates/Python2.jpg',
+    imageUrl: 'images/assets/certificates/Python2.webp',
     icon: null
   },
   {
     title: 'React',
-    imageUrl: 'images/assets/certificates/React.jpg',
+    imageUrl: 'images/assets/certificates/React.webp',
     icon: null
   },
   {
     title: 'Type Script',
-    imageUrl: 'images/assets/certificates/TS.jpg',
+    imageUrl: 'images/assets/certificates/TS.webp',
     icon: null
   },
   {
     title: 'Vue.js (Router, Vuex)',
-    imageUrl: 'images/assets/certificates/Vue.jpg',
+    imageUrl: 'images/assets/certificates/Vue.webp',
     icon: null
   },
   {
     title: 'Vue.js',
-    imageUrl: 'images/assets/certificates/Vue2.jpg',
+    imageUrl: 'images/assets/certificates/Vue2.webp',
     icon: null
   }
 ];
