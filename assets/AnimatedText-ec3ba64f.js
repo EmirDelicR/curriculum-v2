ab7b216d19a74818a6e70533e97a30eaedda2b46
EmirDelicR <EmirDelicR@users.noreply.github.com>
2023-06-18 12:13:59 +0000

    deploy: fc0833ae849358b09bdf326ba74428bcb91fd111

---
+++ assets/AnimatedText-ec3ba64f.js
@@ -0,0 +1,1 @@
+import{e}from"./index-31375d72.js";function m({children:t,isHeadline:a=!1}){return a?e("h1",{className:"animated-text",children:t}):e("p",{className:"animated-text",children:t})}export{m as A};
