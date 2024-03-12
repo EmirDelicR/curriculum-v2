4e9ea13d050cb3c61057df9fafbc4eab9c592302
EmirDelicR <EmirDelicR@users.noreply.github.com>
2024-03-12 09:56:26 +0000

    deploy: e9fb7f9b619df939e545291b5ddf56603c979b2b

---
+++ assets/AnimatedText-99899267.js
@@ -0,0 +1,1 @@
+import{e}from"./index-31a6f2e3.js";function m({children:t,isHeadline:a=!1}){return a?e("h1",{className:"animated-text",children:t}):e("p",{className:"animated-text",children:t})}export{m as A};
--- assets/AnimatedText-b7959cf0.js
+++
@@ -1,1 +0,0 @@
-import{e}from"./index-2bb93bb6.js";function m({children:t,isHeadline:a=!1}){return a?e("h1",{className:"animated-text",children:t}):e("p",{className:"animated-text",children:t})}export{m as A};
