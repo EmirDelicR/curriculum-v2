e9fb7f9b619df939e545291b5ddf56603c979b2b
EmirDelicR <emirdelictbf@gmail.com>
2024-03-12 10:55:02 +0100

    Fix memory leek

--- src/UI/components/textTyping/TextTyping.tsx
+++ src/UI/components/textTyping/TextTyping.tsx
@@ -1,43 +1,22 @@
 import { CSSProperties, useEffect, useState } from 'react';
+import { ReactTyped } from 'react-typed';
 
 import './TextTyping.scss';
 
 const WORDS = ['developer', 'engineer'];
-const TIMING = 4000; // in milliseconds
 
 export default function TextTyping() {
-  const [position, setPosition] = useState(0);
-  let timeout: ReturnType<typeof setInterval>;
-
-  useEffect(() => {
-    timeout = setInterval(() => {
-      setPosition((prev) => {
-        const position = prev + 1;
-        if (position > WORDS.length - 1) {
-          return 0;
-        }
-        return position;
-      });
-    }, TIMING);
-    return () => {
-      clearInterval(timeout);
-    };
-  }, []);
-
   return (
     <div className="text-wrapper">
       <span className="main-text">I&rsquo;m a </span>
-      <span
+      <ReactTyped
         className="typing-text"
-        style={
-          {
-            '--timing': `${TIMING}ms`,
-            '--steps': `${WORDS[position].length}`
-          } as CSSProperties
-        }
-      >
-        {WORDS[position]}
-      </span>
+        startWhenVisible
+        strings={WORDS}
+        typeSpeed={100}
+        backSpeed={50}
+        loop
+      />
     </div>
   );
 }
